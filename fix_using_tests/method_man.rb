112969c6bed679e929a4d1272f7eaaa2610efeda
James Sapp <jameseisapp@gmail.com>
2020-11-03 19:54:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,8 +4,10 @@ def start_game(player1, player2)
   if player1 == player2
    "Hello #{player1} & #{player2}"
 end
+end 
 
 def play_game(player1, player2)
   if player1 == player2
     "#{player1} is better than #{player2}"
 end
+end 
